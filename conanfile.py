5224d79c377763ad6b0c9a092934622629fac643
krzysztof.obrebski <Krzysztof.Obrebski@GAINCapital.com>
2023-05-28 14:21:14 +0200

    fix conan script

--- conanfile.py
+++ conanfile.py
@@ -18,7 +18,7 @@ class StonexCMSAMQPLib(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}  
-    build_requires = ["red-hat-amq-clients-c++/2.10.4@enterprise_messaging/test","jsoncpp/1.9.5@enterprise_mesaging/test","gtest/1.10.0","boost/1.78.0@enterprise_messaging/stable"]
+    build_requires = ["red-hat-amq-clients-c++/2.10.4@enterprise_messaging/test","jsoncpp/1.9.5@enterprise_messaging/test","gtest/1.10.0","boost/1.78.0@enterprise_messaging/stable"]
     generators = "cmake"
     exports_sources = ["include/activemq-cpp/src/main/*"]
     
@@ -46,7 +46,7 @@ class StonexCMSAMQPLib(ConanFile):
     def package(self):
         self.copy("API\*.h", dst="include",src="stonex-cms-amqp\stonex-cms-amqp-lib")
         self.copy("activemq-cpp\src\main\cms\*", dst="include",src="stonex-cms-amqp\stonex-cms-amqp-lib",keep_path=True)
-        self.copy("stonex-cms-amqp-full-lib.lib", dst="lib",src="lib", keep_path=False)
+        self.copy("stonex-cms-amqp-lib.lib", dst="lib",src="lib", keep_path=False)
         self.copy("stonex-cms-amqp-lib.pdb", dst="lib",src="lib", keep_path=False)
         self.copy("*.dll", dst="bin",src="bin", keep_path=False)
 
